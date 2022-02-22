f31d094d199b021b5039d16004b9f16b228f3012
ling <971039649@qq.com>
2022-02-22 14:20:17 +0800

    MVVM实现v-model数据双向绑定

--- MVVM/index.js
+++ MVVM/index.js
@@ -1,10 +1,94 @@
 class MVVM {
   constructor(el, data) {
-    this.el = document.getElementById(el)
-    this._data = data
+    this.el = document.querySelector(el)
+    // this._data = data
+    this.data = data // Proxy 写法
+    this.domPool = {}
     this.init()
   }
-  init() {}
+  init() {
+    this.initData()
+    this.initDom()
+  }
+  initDom() {
+    this.bindDom(this.el)
+    this.bindInput(this.el)
+  }
+  initData() {
+    /**
+     * 1、静态数据转成响应式数据 this.data
+     */
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const _this = this
+    // this.data = {}
+    // for (let key in this._data) {
+    //   Object.defineProperty(this.data, key, {
+    //     get() {
+    //       console.log('获取数据：', key, _this._data[key])
+    //       return _this._data[key]
+    //     },
+    //     set(newVal) {
+    //       console.log('修改数据：', key, newVal)
+    //       _this.domPool[key].innerHTML = newVal
+    //       _this._data[key] = newVal
+    //     }
+    //   })
+    // }
+    // console.log(this.data)
+
+    //Proxy 写法
+    this.data = new Proxy(this.data, {
+      get(target, key) {
+        return Reflect.get(target, key)
+      },
+      set(target, key, val) {
+        _this.domPool[key].innerHTML = val
+        return Reflect.set(target, key, val)
+      }
+    })
+  }
+
+  bindDom(el) {
+    const childNodes = el.childNodes
+
+    childNodes.forEach((item) => {
+      if (item.nodeType === 3) {
+        const _value = item.nodeValue
+        if (_value.trim().length) {
+          let _isValid = /\{\{(.+?)\}\}/.test(_value)
+          if (_isValid) {
+            const _key = _value.match(/\{\{(.+?)\}\}/)[1].trim()
+            this.domPool[_key] = item.parentNode
+            item.parentNode.innerText = this.data[_key] || undefined
+          }
+        }
+      }
+      item.childNodes && this.bindDom(item)
+    })
+  }
+
+  bindInput(el) {
+    /**
+     * 2、绑定事件处理函数，修改数据
+     */
+    const _allInputs = el.querySelectorAll('input')
+    _allInputs.forEach((input) => {
+      const _vModal = input.getAttribute('v-model')
+
+      if (_vModal) {
+        input.addEventListener('keyup', this.handelInput.bind(this, _vModal, input), false)
+      }
+    })
+  }
+  handelInput(key, input) {
+    const _value = input.value
+    this.data[key] = _value
+    console.log(this.data)
+  }
+
+  setData(key, val) {
+    this.data[key] = val
+  }
 }
 
 /**
